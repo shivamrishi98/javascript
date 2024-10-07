e4a0a5cee3ed27c69c4049bf3ab00c7ff0717976
Shivam Rishi <shivamrishisr@gmail.com>
2024-10-07 11:40:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS, not browser
+
+console.log(
+    3
+    +
+    3) // code readability should be high
+
+let name = "Shivam"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (object)
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
