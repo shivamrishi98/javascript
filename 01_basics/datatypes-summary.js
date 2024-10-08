9800134721f2fc231d79b4ab7db3b9bb49fa6e23
Shivam Rishi <shivamrishisr@gmail.com>
2024-10-08 04:55:16 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive Data Type
+// call by value
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Javascipt is a dynamic typed language
+
+const bigNumber = 34265352625n
+
+
+// Non-primitive Data Type (Reference type)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+
+// Objects
+let myObj = {
+    name: "Shivam",
+    age: 22
+}
+
+const myFunction = function() {
+    console.log("Hello world");
+}
